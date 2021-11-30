67f65d4885abdd4c6815e290af34284c6ef10ba6
tablu666 <tablu666@outlook.com>
2021-11-30 16:32:22 -0500

    add OperatorTest

---
+++ src/OperatorTest.java
@@ -0,0 +1,79 @@
+import student.TestCase;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The purpose of this class is to test
+ * the methods of class Operator.
+ *
+ * @author Tianbo Lu & Yuechen Feng
+ * @version 2021-11-30
+ */
+public class OperatorTest extends TestCase {
+
+    private Operator opr;
+    private RandomAccessFile file;
+    private int heapSize = 65536;
+    private MinHeap<Record> minHeap;
+    private List<RunInfo> runInfoList;
+
+    @Override
+    public void setUp() throws Exception {
+        String[] input = {"opr.bin", "9"};
+        reversed(input);
+        this.file = new RandomAccessFile("opr.bin", "rw");
+        this.opr = new Operator(file);
+
+        int recordSize = 16;
+        int numOfRecord = heapSize / recordSize;
+        Record[] records = IOHelper.readRecords(file, 0, numOfRecord);
+        this.minHeap = new MinHeap<>(records, numOfRecord, numOfRecord);
+        this.runInfoList = new ArrayList<>();
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        this.file.close();
+    }
+
+    public void testReplacementSelection() {
+        try {
+            RandomAccessFile runFile = opr.replacementSelection(runInfoList, minHeap, heapSize);
+            assertEquals(file.length(), runFile.length());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void testMultiWayMerge() {
+        try {
+            RandomAccessFile runFile = opr.replacementSelection(runInfoList, minHeap, heapSize);
+            opr.multiWayMerge(runFile, minHeap.getData(), runInfoList);
+            assertEquals(8192 * 9, file.getFilePointer());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void reversed(String[] args) throws IOException {
+        long val;
+        double val2;
+        int filesize = Integer.parseInt(args[1]); // Size of file in blocks
+        DataOutputStream file = new DataOutputStream(new BufferedOutputStream(
+                new FileOutputStream(args[0])));
+        // For sorted.
+
+        for (int i = filesize - 1; i >= 0; i--) {
+            for (int j = 512 - 1; j >= 0; j--) {
+                val = (long)(i * 512 + j);
+                file.writeLong(val);
+                val2 = (double)(i * 512 + j);
+                file.writeDouble(val2);
+            }
+        }
+        file.flush();
+        file.close();
+    }
+}
--- src/RunInfoTest.java
+++ src/RunInfoTest.java
@@ -1,9 +1,17 @@
 import student.TestCase;
 
+/**
+ * The purpose of this class is to test
+ * the methods of class RunInfo.
+ *
+ * @author Tianbo Lu & Yuechen Feng
+ * @version 2021-11-30
+ */
 public class RunInfoTest extends TestCase {
 
     private RunInfo runInfo;
 
+    @Override
     public void setUp() throws Exception {
         this.runInfo = new RunInfo(0, 0);
     }
